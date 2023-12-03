c7ded9b261c50bf4c87995fb4b6ee3cbdc710c2b
Maryamraeesalvi01 <maryamalvi133@gmail.com>
2023-12-04 00:13:53 +0500

    StackTrace,Update product button,Add to cart button(Resolved Bugs)

--- src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
+++ src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
-import com.jtspringproject.JtSpringProject.services.cartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
