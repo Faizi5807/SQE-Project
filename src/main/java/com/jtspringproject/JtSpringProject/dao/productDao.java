53ca5a4bc8fa5574ac528b6acd9df91ac68c7218
Maryamraeesalvi01 <maryamalvi133@gmail.com>
2023-12-03 22:53:38 +0500

    "Product Update button fixed!"

--- src/main/java/com/jtspringproject/JtSpringProject/dao/productDao.java
+++ src/main/java/com/jtspringproject/JtSpringProject/dao/productDao.java
@@ -14,30 +14,30 @@ import com.jtspringproject.JtSpringProject.models.Product;
 @Repository
 public class productDao {
 	@Autowired
-    private SessionFactory sessionFactory;
-	
+	private SessionFactory sessionFactory;
+
 	public void setSessionFactory(SessionFactory sf) {
-        this.sessionFactory = sf;
-    }
-	
+		this.sessionFactory = sf;
+	}
+
 	@Transactional
 	public List<Product> getProducts(){
 		return this.sessionFactory.getCurrentSession().createQuery("from PRODUCT").list();
 	}
-	
+
 	@Transactional
 	public Product addProduct(Product product) {
 		this.sessionFactory.getCurrentSession().save(product);
 		return product;
 	}
-	
+
 	@Transactional
 	public Product getProduct(int id) {
 		return this.sessionFactory.getCurrentSession().get(Product.class, id);
 	}
-
+	@Transactional
 	public Product updateProduct(Product product){
-		this.sessionFactory.getCurrentSession().update(String.valueOf(Product.class),product);
+		this.sessionFactory.getCurrentSession().update(product);
 		return product;
 	}
 	@Transactional
