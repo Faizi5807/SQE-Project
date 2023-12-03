89d61b7dd68e7e5cbc570d9d166ec9620e93e4aa
Faizi5807 <l215807@lhr.nu.edu.pk>
2023-12-04 00:02:41 +0500

    Solved Bugs push

--- src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
+++ src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
@@ -4,12 +4,7 @@ import com.jtspringproject.JtSpringProject.models.Cart;
 import com.jtspringproject.JtSpringProject.models.Product;
 import com.jtspringproject.JtSpringProject.models.User;
 
-import java.io.Console;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+
 import java.util.ArrayList;
 import java.util.List;
 
